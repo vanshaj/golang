e375ea7a2e95f9bbb3d240125e3ad80dd7b922d9
Vanshaj <himanshudhar6@gmail.com>
2021-06-05 13:24:45 +0530

    created a simple http get request

---
+++ blackhatgo/chapter3/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	var client *http.Client = &http.Client{}
+	baseurl := "https://reqres.in/api/"
+	endpoint := "users?page=2"
+
+	req, err := http.NewRequest("GET", baseurl+endpoint, nil)
+	if err != nil {
+		fmt.Println("Unable to create connection to ", baseurl, " because of ", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("Unable to send request ", err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Unable to read body")
+		return
+	}
+	fmt.Println(string(body))
+}
