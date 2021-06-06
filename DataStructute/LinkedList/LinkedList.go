6b9f5151ab16c3c76fc7008025d3603b74b3b333
vanshaj <himanshudhar6@gmail.com>
2021-06-06 13:49:13 +0530

    Added code to add item at first place in linkedlist

--- DataStructute/LinkedList/LinkedList.go
+++ DataStructute/LinkedList/LinkedList.go
@@ -4,13 +4,39 @@ import (
 	"fmt"
 )
 
-type node struct{
-	Data int
-	Pointer *node
+type node struct {
+	data int
+	next *node
 }
 
-type 
+type LinkedList struct {
+	head *node
+	size int
+}
 
-func main(){
-	
+func (ll *LinkedList) prepend(n *node) {
+	n.next = ll.head
+	ll.head = n
+	ll.size++
+}
+
+func (ll LinkedList) printList() {
+	node := ll.head
+	for ll.size != 0 {
+		fmt.Println(node.data)
+		node = node.next
+		ll.size--
+	}
+}
+
+func main() {
+	firstElem := node{1, nil}
+	head := &firstElem
+	ll := LinkedList{head, 1}
+	node1 := node{2, nil}
+	ll.prepend(&node1)
+	ll.prepend(&node{3, nil})
+	ll.prepend(&node{4, nil})
+	node1.data = 6
+	ll.printList()
 }
