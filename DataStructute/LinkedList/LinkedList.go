e04b93504a5944b3f73e8fc43e568a7d77a6c36a
Vanshaj <himanshudhar6@gmail.com>
2021-06-10 15:43:25 +0530

    added delete and add operations

--- DataStructute/LinkedList/LinkedList.go
+++ DataStructute/LinkedList/LinkedList.go
@@ -20,9 +20,62 @@ func (ll *LinkedList) prepend(n *node) {
 	ll.size++
 }
 
+func (ll *LinkedList) apppend(n *node) {
+	node := ll.head
+	for {
+		if node.next != nil {
+			node = node.next
+			continue
+		}
+		node.next = n
+		break
+	}
+	ll.size++
+}
+
+func (ll *LinkedList) addAtPosition(n *node, pos int) {
+	node := ll.head
+	counter := 0
+	if pos == 0 {
+		n.next = node
+		ll.head = n
+	} else {
+		for counter < ll.size {
+			if counter == pos-1 {
+				n.next = node.next
+				node.next = n
+				break
+			}
+			node = node.next
+			counter++
+		}
+	}
+	ll.size++
+}
+
+func (ll *LinkedList) DeleteAtStart() {
+	deleteNode := ll.head
+	ll.head = deleteNode.next
+	ll.size--
+}
+
+func (ll *LinkedList) DeleteAtEnd() {
+	node := ll.head
+	for {
+		if node.next.next == nil {
+			node.next = nil
+			ll.size--
+			break
+		} else {
+			node = node.next
+		}
+
+	}
+}
+
 func (ll LinkedList) printList() {
 	node := ll.head
-	for ll.size != 0 {
+	for node != nil {
 		fmt.Println(node.data)
 		node = node.next
 		ll.size--
@@ -37,6 +90,13 @@ func main() {
 	ll.prepend(&node1)
 	ll.prepend(&node{3, nil})
 	ll.prepend(&node{4, nil})
-	node1.data = 6
+	ll.apppend(&node{5, nil})
+	ll.addAtPosition(&node{6, nil}, 1)
+	ll.printList()
+	fmt.Println("------------")
+	ll.DeleteAtStart()
+	ll.printList()
+	fmt.Println("------------")
+	ll.DeleteAtEnd()
 	ll.printList()
 }
